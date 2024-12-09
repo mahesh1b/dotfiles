94ecfe272e77f5fec434923fdc5c14bc083190f4
Mahesh Bhosle <mahesh@MyMacBookAir.local>
2024-12-09 14:50:37 +0530

    Added the lang server in lspconf file

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls" }
+local servers = { "html", "cssls", "terraformls", "yamlls" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
