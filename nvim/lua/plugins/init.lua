2824d5ef4282fdf303968cc7d2788b0c33fbf9f0
Mahesh Bhosle <mahesh@MyMacBookAir.local>
2024-12-09 14:50:17 +0530

    Insatlled nvim-lspconf, Mason and treesitter with tf and yaml

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -2,23 +2,42 @@ return {
   {
     "stevearc/conform.nvim",
     -- event = 'BufWritePre', -- uncomment for format on save
-    opts = require "configs.conform",
+    config = function()
+      require "configs.conform"
+    end,
   },
 
-  -- These are some examples, uncomment them if you want to see them work!
   {
     "neovim/nvim-lspconfig",
     config = function()
+      require("nvchad.configs.lspconfig").defaults()
       require "configs.lspconfig"
     end,
   },
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+  
+  {
+   "williamboman/mason.nvim",
+   opts = {
+    ensure_installed = {
+      "lua-language-server",
+      "stylua",
+      "terraformls",
+      "yamlls",
+      "prettier",
+    },
+   },
+  },
+  
+  {
+   "nvim-treesitter/nvim-treesitter",
+   opts = {
+    ensure_installed = {
+      "vim",
+      "lua",
+      "vimdoc",
+      "terraform",
+      "yaml",
+    },
+   },
+  },
 }
