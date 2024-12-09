953bcc15e8efa0489eec8432a0a881a0f98b32d4
Mahesh Bhosle <mahesh@MyMacBookAir.local>
2024-12-09 09:13:45 +0530

    Added keymapping for term

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -7,4 +7,17 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
+-- new terminals
+map("n", "<leader>h", function()
+  require("nvchad.term").new { pos = "sp" }
+end, { desc = "terminal new horizontal term" })
+
+map("n", "<leader>v", function()
+  require("nvchad.term").new { pos = "vsp" }
+end, { desc = "terminal new vertical term" })
+
+map("n", "<leader>tf", function()
+  require("nvchad.term").toggle { pos = "float" }
+end, { desc = "terminal toggle floating term" })
+
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
