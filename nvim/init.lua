6ac4582bd26777128a4410f6ef6ee031fba8c2f0
Mahesh Bhosle <mahesh@MyMacBookAir.local>
2024-12-10 09:29:20 +0530

    Added fix for tf commenting

--- nvim/init.lua
+++ nvim/init.lua
@@ -35,3 +35,12 @@ require "nvchad.autocmds"
 vim.schedule(function()
   require "mappings"
 end)
+
+--fix terraform and hcl comment string
+vim.api.nvim_create_autocmd("FileType", {
+  group = vim.api.nvim_create_augroup("FixTerraformCommentString", { clear = true }),
+  callback = function(ev)
+    vim.bo[ev.buf].commentstring = "# %s"
+  end,
+  pattern = { "terraform", "hcl" },
+})
